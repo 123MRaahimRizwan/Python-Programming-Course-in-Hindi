e15d4dc1afcebae6bfb0b083258ed3a86c8bba14
M. Raahim Rizwan <87467799+123MRaahimRizwan@users.noreply.github.com>
2022-10-07 09:52:03 +0500

    Initial commit

---
+++ input_function.py
@@ -0,0 +1,8 @@
+# print(input("Please enter your name: "))
+
+# inp = input("Enter your name: ")
+# print("Hello ", inp)
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+print("The product is" , num1 * num2)
