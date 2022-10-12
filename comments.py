ce65228a329fb22198ea6daae76e69bd470affe3
M. Raahim Rizwan <87467799+123MRaahimRizwan@users.noreply.github.com>
2022-10-12 13:13:12 +0500

    Initial commit

---
+++ comments.py
@@ -0,0 +1,11 @@
+# print("This is a dog")
+'''print("This is a programming tutorial")
+print("This is a dog")
+print("This is a programming tutorial")
+print("This is a dog")''' # These are also called Docstrings
+
+print("This is a dog")
+
+# def sum():
+#     """This function adds two numbers"""
+#     pass
