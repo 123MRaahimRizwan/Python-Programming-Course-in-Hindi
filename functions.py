9b71b6bfb66081ef3431b1a04b73e35fd657f543
M. Raahim Rizwan <87467799+123MRaahimRizwan@users.noreply.github.com>
2022-10-22 12:20:47 +0500

    Initial commit

---
+++ functions.py
@@ -0,0 +1,24 @@
+list1 = [1,2,4,5,6,8]
+print(len(list1))
+
+def function1():
+    print("This is inside function1")
+
+function1()
+
+"""This is a function which provides the average of two numbers."""
+def avg(a,b):
+    """This is a function which provides the average of two numbers."""
+    average = (a+b)/2
+    # print(average)
+    return average
+
+var = avg(4,7)
+print(var)
+
+print(sum((5,7)))
+print(avg.__doc__)
+
+# Tuples (1,23,5,6,8)
+# List [1,23,5,6,8]
+# Variables = 1,23,5,8,6
